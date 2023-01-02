5037c8b3a924c569d8eaae3459601a39c611a6fb
aguili <h.aguili@spinalcom.com>
2023-01-02 11:26:26 +0100

    install vuex && store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,19 @@
 /*
  * Copyright 2023 SpinalCom - www.spinalcom.com
- * 
+ *
  * This file is part of SpinalCore.
- * 
+ *
  * Please read all of the following terms and conditions
  * of the Free Software license Agreement ("Agreement")
  * carefully.
- * 
+ *
  * This Agreement is a legally binding contract between
  * the Licensee (as defined below) and SpinalCom that
  * sets forth the terms and conditions that govern your
  * use of the Program. By installing and/or using the
  * Program, you agree to abide by all the terms and
  * conditions stated or referenced herein.
- * 
+ *
  * If you do not agree to abide by these terms and
  * conditions, do not demonstrate your acceptance and do
  * not install or use the Program.
@@ -22,25 +22,13 @@
  * <http://resources.spinalcom.com/licenses.pdf>.
  */
 
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
-import router from './router';
-import vuetify from '@/plugins/vuetify'; // path to vuetify export
-import VueCompositionAPI from '@vue/composition-api';
-import Vuelidate from 'vuelidate';
-Vue.use(Vuelidate);
-Vue.use(VueCompositionAPI);
-Vue.config.productionTip = false;
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
 
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>',
-  vuetify,
-  validations: {}
-});
+export default new Vuex.Store({
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+})
