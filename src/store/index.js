f0b4ff9e2c6da875ab74e57e2fba60aaefaab71a
aguili <h.aguili@spinalcom.com>
2023-01-02 14:51:06 +0100

    add vuex store state

--- src/store/index.js
+++ src/store/index.js
@@ -24,11 +24,12 @@
 
 import Vue from 'vue'
 import Vuex from 'vuex'
+import module from '../modules/module'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: { module },
 })
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,30 +1,108 @@
+<!--
+Copyright 2023 SpinalCom - www.spinalcom.com
+
+This file is part of SpinalCore.
+
+Please read all of the following terms and conditions
+of the Free Software license Agreement ("Agreement")
+carefully.
+
+This Agreement is a legally binding contract between
+the Licensee (as defined below) and SpinalCom that
+sets forth the terms and conditions that govern your
+use of the Program. By installing and/or using the
+Program, you agree to abide by all the terms and
+conditions stated or referenced herein.
+
+If you do not agree to abide by these terms and
+conditions, do not demonstrate your acceptance and do
+not install or use the Program.
+You should have received a copy of the license along
+with this file. If not, see
+<http://resources.spinalcom.com/licenses.pdf>.
+-->
+
 <template>
   <v-app class="app">
-    <div class="d-flex justify-end" style="width: 100% ; min-width: 980px;">
-      <v-card class="d-flex flex-column ml-2 pl-1 pt-1 pb-1 pr-1 justify-center rounded-lg" elevation="2">
-        <BlueButton @click.native="adduser()" :icon="'mdi-plus'" title="AJOUTER UN UTILISATEUR" :val="'blue'" />
+    <!-- <ul class="list-group mt-5">
+      <li
+        class="list-group-item list-group-item-action"
+        v-for="data in dataList"
+        :key="data.dynamicId"
+      >
+        <h3>{{ data.name }}</h3>
+      </li>
+    </ul> -->
+
+    <div class="d-flex justify-end" style="width: 100%; min-width: 980px">
+      <v-card
+        class="
+          d-flex
+          flex-column
+          ml-2
+          pl-1
+          pt-1
+          pb-1
+          pr-1
+          justify-center
+          rounded-lg
+        "
+        elevation="2"
+      >
+        <BlueButton
+          @click.native="adduser()"
+          :icon="'mdi-plus'"
+          title="AJOUTER UN UTILISATEUR"
+          :val="'blue'"
+        />
       </v-card>
     </div>
     <BackupInformation title="TABLES DES UTILISATEURS">
       <div class="d-flex mb-2 mt-4">
-        <div style="width: 22%;">Nom d'utilisateur</div>
+        <div style="width: 22%">Nom d'utilisateur</div>
         <div style="width: 78%">État</div>
       </div>
       <div v-for="item in userList" :key="item.id">
         <div class="d-flex mb-2">
-          <div style="width: 22%;" class="content-list rounded-l-lg pl-10">{{ item.userName }}</div>
-          <div style="width: 78% ;" class="content-list">
+          <div style="width: 22%" class="content-list rounded-l-lg pl-10">
+            {{ item.userName }}
+          </div>
+          <div style="width: 78%" class="content-list">
             <div class="stateButton-container">
-              <StateButton :obj="'bos'" :content1="'BOS 1'" :content2="'ÉTAGE 24'" :icon="'mdi-chip'" />
-              <StateButton :obj="'app'" :content1="'APPLICATION'" :content2="'PROFIL 10'" :icon="'mdi-apps'" />
+              <StateButton
+                :obj="'bos'"
+                :content1="'BOS 1'"
+                :content2="'ÉTAGE 24'"
+                :icon="'mdi-chip'"
+              />
+              <StateButton
+                :obj="'app'"
+                :content1="'APPLICATION'"
+                :content2="'PROFIL 10'"
+                :icon="'mdi-apps'"
+              />
             </div>
             <div class="stateButton-container">
-              <StateButton :obj="'bos'" :content1="'BOS 1'" :content2="'ÉTAGE 24'" :icon="'mdi-chip'" />
-              <StateButton :obj="'app'" :content1="'APPLICATION'" :content2="'PROFIL 10'" :icon="'mdi-apps'" />
+              <StateButton
+                :obj="'bos'"
+                :content1="'BOS 1'"
+                :content2="'ÉTAGE 24'"
+                :icon="'mdi-chip'"
+              />
+              <StateButton
+                :obj="'app'"
+                :content1="'APPLICATION'"
+                :content2="'PROFIL 10'"
+                :icon="'mdi-apps'"
+              />
             </div>
           </div>
           <div class="content-list rounded-r-lg hover">
-            <button class="pr-2" style="height: 100%;" @click="displayDetail(item)">
+            <button
+              class="pr-2"
+              style="height: 100%"
+              @click="displayDetail(item)"
+            >
               <v-icon>mdi-arrow-right</v-icon>
             </button>
           </div>
@@ -33,11 +111,13 @@
     </BackupInformation>
   </v-app>
 </template>
-  
+
 <script>
-import BlueButton from "../Components/BlueButton.vue"
-import BackupInformation from "../Components/BackupInformation.vue"
-import StateButton from "../Components/StateButton.vue"
+import BlueButton from "../Components/BlueButton.vue";
+import BackupInformation from "../Components/BackupInformation.vue";
+import StateButton from "../Components/StateButton.vue";
+import { mapActions, mapGetters } from "vuex";
+import { mapState } from "vuex";
 
 export default {
   name: "App",
@@ -52,23 +132,32 @@ export default {
     userList: [
       {
         id: 1,
-        userName: 'gab du bour'
+        userName: "gab du bour"
       }
-    ],
+    ]
   }),
 
   methods: {
+    getData() {
+      this.$store.dispatch("module/fetchData");
+      console.log(this.$store.dispatch("module/fetchData"));
+    },
     displayDetail(item) {
       this.$router.push("/DetailUser");
     },
     adduser(item) {
       this.$router.push("/AddUser");
     }
+  },
+  computed: {
+    ...mapState(["module"])
+  },
+  created() {
+    this.getData();
   }
-}
-
+};
 </script>
-  
+
 <style scoped>
 .app {
   font: normal normal normal 10px/12px Charlevoix Pro;
@@ -95,6 +184,6 @@ export default {
 
 .stateButton-container {
   width: 100%;
-  display: flex
+  display: flex;
 }
 </style>
